0eb8d58100fbd53d5a4005c36f1f97f327146b1b
Techvolutions05 <anell.santos05@gmail.com>
2022-08-21 19:14:54 +0200

    A magic 8 ball application built with flutter for practice and learning purpose...

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,52 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+          home: Scaffold(
+        backgroundColor: Colors.blue.shade400,
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade800,
+          title: Center(
+            child: Text('Ask Me Anything'),
+          ),
+        ),
+        body: MagicBall(),
+      )),
+    );
+
+class MagicBall extends StatefulWidget {
+  @override
+  State<MagicBall> createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballface = 1;
+  @override
+  Widget build(BuildContext context) {
+    void changeballface() {
+      setState(
+        () {
+          ballface = Random().nextInt(5) + 1;
+        },
+      );
+    }
+
+    return Center(
+      child: Column(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeballface();
+              },
+              child: Image(
+                image: AssetImage('images/ball$ballface.png'),
+              ),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
